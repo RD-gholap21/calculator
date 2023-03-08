d25db1b9454c4493a3bf8d734f3933ce166d1f12
Viraj Gholap <rohitgholap125@gmail.com>
2023-03-08 18:37:12 +0530

    add project file

---
+++ calculator.js
@@ -0,0 +1,44 @@
+var field = document.getElementsByClassName('id')[0];
+
+
+
+function number(Number) {
+
+    if (field.value == '0') {
+        field.value = Number;
+    } else {
+        field.value += Number;
+    }
+}
+
+function allclear() {
+    // alert('hii');
+    field.value = '0';
+}
+
+function Erase() {
+
+    var del = field.value;
+    var z = 'rohitgholap';
+    if (del.length > 1) {
+        field.value = del.slice(0, -1);
+    } else {
+        field.value = '0'
+    }
+}
+
+
+function sign() {
+    solve = field.value;
+    if (solve >= '0') {
+
+        field.value = eval(solve)
+    } else {
+        field.value = "0"
+    }
+}
+
+function History() {
+    var clear = document.getElementById('history');
+    clear.innerHTML += field.value + '<br>' + solve + '=' + '<br>';
+}
